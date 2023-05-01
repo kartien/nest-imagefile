227f6686cd56063bff4e4bf3cad355c5840c36df
kartien <kevinramon523@gmail.com>
2023-04-30 21:48:44 -0500

    Save image on local cwd

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,4 +1,19 @@
-export async function GET(request) {
-    return new Response('Uploading file')
-  }
-  
+import { writeFile } from "fs/promises";
+import path from "path";
+
+export async function POST(request) {
+  const data = await request.formData();
+  const file = data.get("file");
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const filePath = path.join(process.cwd(), 'public', file.name)
+  writeFile(filePath, buffer)
+  console.log('File upload to', filePath)
+
+  return new Response(
+    JSON.stringify({
+      message: "Uploading file!",
+    })
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,30 @@
 import { useState } from "react";
 
 function HomePage() {
-  
   const [file, setFile] = useState();
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        if (!file) return
-        console.log('updating image')
-      }}>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          if (!file) return;
+          const form = new FormData()
+          form.set('file', file)
+          // sending file
+          const res = await fetch("/api/upload", {
+            method: "POST",
+            body: form
+          });
+          const data = await res.json()
+          console.log(data);
+        }}
+      >
         <label>Upload file:</label>
         <input
           type="file"
           onChange={(e) => {
-            setFile(e.target.files[0])
+            setFile(e.target.files[0]);
           }}
         />
         <button>Upload</button>
