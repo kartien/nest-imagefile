f242e50fb39ecf481d2d1ddb126c65e30c92ac5f
kartien <kevinramon523@gmail.com>
2023-04-30 23:11:23 -0500

    Add image preview card

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,19 +1,33 @@
 import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
 import path from "path";
 
 export async function POST(request) {
-  const data = await request.formData();
-  const file = data.get("file");
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
+  try {
+    
+    const data = await request.formData();
+    const file = data.get("file");
 
-  const filePath = path.join(process.cwd(), 'public', file.name)
-  writeFile(filePath, buffer)
-  console.log('File upload to', filePath)
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-  return new Response(
-    JSON.stringify({
-      message: "Uploading file!",
-    })
-  );
+    const filePath = path.join(process.cwd(), "public", file.name);
+    writeFile(filePath, buffer);
+    console.log("File upload to", filePath);
+
+    return new Response(
+      JSON.stringify({
+        message: "Uploading file!",
+      })
+    );
+  } catch (error) {
+    return NextResponse.json(
+      JSON.stringify({
+        message: "no file",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,62 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 function HomePage() {
   const [file, setFile] = useState();
 
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+    try {
+      const form = new FormData();
+      form.set("file", file);
+      // sending file
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: form,
+      });
+      if (res.ok) {
+        console.log("fle uploaded");
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          if (!file) return;
-          const form = new FormData()
-          form.set('file', file)
-          // sending file
-          const res = await fetch("/api/upload", {
-            method: "POST",
-            body: form
-          });
-          const data = await res.json()
-          console.log(data);
-        }}
-      >
-        <label>Upload file:</label>
-        <input
-          type="file"
-          onChange={(e) => {
-            setFile(e.target.files[0]);
-          }}
-        />
-        <button>Upload</button>
-      </form>
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-zinc-950 p-5">
+        <h1 className="text-4xl text-center my-4">Upload File</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
+            onChange={handleFileChange}
+          />
+          <button
+            className="bg-green-500 text-zinc-100 p-2 rounded block w-full disabled:opacity-50"
+            disabled={!file}
+          >
+            Upload
+          </button>
+        </form>
+        {
+          file && (
+            <Image src={URL.createObjectURL(file)} alt="upload file"
+              className="w-64 h-65 object-cover mx-auto"
+              width={256}
+              height={256}
+            />
+          )
+        }
+      </div>
     </div>
   );
 }
